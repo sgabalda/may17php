3c84431f0bad7f2d5a2b87d0a39eac87a7a55fc8
PHP i MySql <phpapps@aula.local>
2017-05-15 11:20:13 +0200

    Formularios

--- www/formularios/formGenerator.php
+++ www/formularios/formGenerator.php
@@ -6,8 +6,47 @@
  * string html
  **/
 
-funtion formGenerator($form)
+function formGenerator($form)
 {
+  $html='';
   echo $form;
+
+  $jsonForm = file_get_contents($form);
+  $jsonArray = json_decode($jsonForm, true);
+
+
+  echo "<pre>";
+  print_r($jsonArray);
+  echo "</pre>";
+
+
+  // Abrir el formulario
+    // Para cada elemento del formulario
+      // Si es Text
+        // Contruir el campo de tipo texto
+      // Si es hidden
+        // construir el campo
+
+      // Si es textarea
+        // construir el campo
+      // Si es checkbox
+        // construir el campo
+      // Si es select
+      // Si es selectmultiple
+      // Si es mail
+      // Si es date
+      // Si es password
+      // Si es file
+      // Si es radio
+
+
+  // Cerrar formulario
+
+
+
+
+
+
+
   return $html;
 }
