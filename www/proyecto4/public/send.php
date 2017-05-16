d36cb1f8168f434f5fe4e95c50d6febd219ff779
Agustincl <agustincl@gmail.com>
2017-05-16 13:10:22 +0200

    Users por comas

--- www/proyecto4/public/send.php
+++ www/proyecto4/public/send.php
@@ -6,15 +6,31 @@ echo "</pre>";
 
 // Guardar en un archivo separado por comas
 
+$line = array();
 // para cada elemento del array POST
-
-      // Si el elemento es de tipo array
-          // Separ por pipes
-
-     // Si el elemento no es de tipo ArrayAccess
+foreach($_POST as $value)
+{
+      // Si el elemento no es de tipo array
+      if(!is_array($value))
+      {
+          // Separ por comas
+          $line[]= $value;
+      }
+      // Si el elemento es de tipo ArrayAccess
+      else
+      {
           // Separa por comas
+          $line[]= implode('|',$value);
+      }
+}
 
-     // Concatenar con el elemento anterior
-
-
+// Concatenar con el elemento anterior
+$line = implode(',', $line)."\n";
 // Ver por pantalla la linea
+echo "<pre>";
+print_r($line);
+echo "</pre>";
+
+
+
+file_put_contents('user.txt', $line, FILE_APPEND);
---
+++ www/proyecto4/public/userList.php
@@ -0,0 +1,31 @@
+<?php
+
+$filename = 'user.txt';
+
+// Leer los datos del archivo en un string
+$users = file_get_contents($filename);
+
+// Separar por saltos de linea el string en un array
+$users = explode("\n", $users);
+
+$matriz = array();
+// Para cada elemento del array
+foreach($users as $key => $user)
+{
+    //Separ la fila por comas
+    $matriz[]=explode(',', $user);
+}
+
+
+// llamar a dibuja tabla con la matriz
+echo "<pre>";
+print_r($matriz);
+echo "</pre>";
+
+
+require('../vendor/cervezza/Utils/src/Utils/dibujaTabla.php');
+
+$html = dibujaTabla($matriz);
+
+
+echo $html;
