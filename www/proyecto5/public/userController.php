df6c56f6f9896d9500177c807fea2ae08294d181
Agustincl <agustincl@gmail.com>
2017-05-17 09:59:45 +0200

    Crud users file

--- www/proyecto5/public/userController.php
+++ www/proyecto5/public/userController.php
@@ -1,18 +1,61 @@
 <?php
 
+echo "<pre>GET: ";
+print_r($_GET);
+echo "</pre>";
+
+echo "<pre>POST: ";
+print_r($_POST);
+echo "</pre>";
+
+require('../vendor/cervezza/Utils/src/Utils/dibujaTabla.php');
+require('../vendor/cervezza/FormGenerator/src/FormGenerator/Model/formGenerator.php');
+
 
 if(isset($_GET['action']))
   $action = $_GET['action'];
 else
   $action = 'select';
 
+$usersFilename = '../modules/UserRegister/src/UserRegister/Model/Data/users.txt';
+
 switch ($action)
 {
   case 'select':
     echo "esto es select";
+    echo "<a href=\"/userController.php?action=insert\">Insert</a>";
+    $users = file_get_contents($usersFilename);
+    $users = explode("\n", $users);
+    foreach($users as $key => $user)
+        $users[$key]=explode(',', $user);
+    $html = dibujaTabla($users);
+    echo $html;
   break;
   case 'insert':
     echo "esto es insert";
+    $form = "../modules/UserRegister/src/UserRegister/Model/Forms/user.json";
+    if($_POST)
+    {
+      // Procesar formulario
+      echo "<pre>procesar esto: ";
+      $line = array();
+      foreach($_POST as $value)
+      {
+            if(!is_array($value))
+                $line[]= $value;
+            else
+                $line[]= implode('|',$value);
+      }
+      $line = implode(',', $line)."\n";
+      file_put_contents($usersFilename, $line, FILE_APPEND);
+      header("Location: /userController.php?action=select");
+    }
+    else
+    {
+      $html = formGenerator($form);
+      echo $html;
+    }
+
   break;
   case 'update':
     echo "esto es update";
@@ -21,6 +64,6 @@ switch ($action)
     echo "esto es delete";
   break;
   default:
-    echo "esto es todos los demas";
+    echo "error 404";
   break;
 }
