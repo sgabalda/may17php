b29c3b7d2cbb745fcd2dad29bda870d4d938ab39
Agustincl <agustincl@gmail.com>
2017-05-23 10:03:24 +0200

    Router

--- www/proyecto5/vendor/cervezza/Utils/src/Utils/Router.php
+++ www/proyecto5/vendor/cervezza/Utils/src/Utils/Router.php
@@ -4,6 +4,15 @@
  * Set route from url
  * string url
  * array route
+ *
+ * /users/delete/iduser/8         ---> ok
+ * /users/delete/iduser/8/kaka    ---> error
+ * /users/delete/iduser           ---> error
+ * /users/delete                  ---> ok
+ * /users/kaka                    ---> error
+ * /users                         ---> ok
+ * /kaka                          ---> error
+ * /                              ---> ok
  */
 
 function Router($url='/')
@@ -17,17 +26,18 @@ function Router($url='/')
   echo "</pre>";
 
   $router['module']='default';
-  $router['module']='default';
-  $router['module']='default';
-  $router['module']='default';
 
-  $router=array('module'=>'default',
-                'controller'=>'users',
-                'action'=>'delete',
-                'params'=>array('param1'=>'val1',
-                                'param1'=>'val1',
-                                'param1'=>'val1')
-  );
+  $router['controller']=$url[1];
+  $router['action']=$url[2];
+  $router['params']=array($url[3]=>$url[4]);
+
+  // $router=array('module'=>'default',
+  //               'controller'=>'users',
+  //               'action'=>'delete',
+  //               'params'=>array('param1'=>'val1',
+  //                               'param1'=>'val1',
+  //                               'param1'=>'val1')
+  // );
   echo "<pre>";
   print_r($router);
   echo "</pre>";
